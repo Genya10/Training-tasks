d90f595d182fdc50810fde034dbd1f718971f51c
Jenya <113167825+Genya10@users.noreply.github.com>
2024-02-11 12:47:16 +0200

    create css and filling js

--- Input Phone Animation/script.js
+++ Input Phone Animation/script.js
@@ -1,2 +1,14 @@
-//const progressBar = document.querySelector('progress');
-//progressBar.value = 75; 
+const inputId = document.querySelector('#input'); 
+const line = document.querySelector('.line');
+
+IMask(
+    inputId,
+    {mask:'+{38 (\\000) 000-00-00}'}
+)
+
+inputId.addEventListener("input",function(){
+   const width = this.offsetWidth;
+   const valueLength = this.value.length;
+   line.style.width = ((width / 19) * valueLength) + 'px';
+   line.style.backgroundColor = `rgb(${(255 - (255/19) * valueLength)}, 130,0)`;
+})
