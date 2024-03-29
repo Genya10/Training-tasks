996c7f5c13cecfaed5524fc9987f511e5aaeebfd
Jenya <113167825+Genya10@users.noreply.github.com>
2024-03-29 12:14:36 +0200

    add promise with family

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,5 +1,5 @@
 ///////////////////////////////
-async function getId() {
+/*async function getId() {
   const resp = await fetch('https://jsonplaceholder.typicode.com/todos/5')
   console.log(resp);
   const get = await resp.json()
@@ -15,4 +15,60 @@ console.log(countries)
 })
 .catch(error => console.log(error))
 
-console.log("LA-LA-LA")
+console.log("LA-LA-LA");*/
+
+/////////////////////////
+/*const coffee = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+     resolve('Coffffeeeee....')
+     reject(Error('Coffee is done!'))
+    },2000)    
+});
+
+coffee.then(data=>{
+    console.log(data)
+}).catch(error=>console.log(error));*/
+
+///////////////////////////////////////////
+const family = [
+    { member: 'mother', id: 1, coffee: 'Latte' },
+    { member: 'father', id: 2, coffee: 'Espresso' },
+    { member: 'son', id: 3, coffee: 'Cappucino' },
+]
+
+const getCoffee =(member)=>{
+ const coffeePromise = fetch('https://api.sampleapis.com/coffee/hot');
+ return coffeePromise
+ .then(data => data.json())
+ .then(list=>{
+    console.log('list:',list)
+    const coffee = list.find(res => res.title === member.coffee)
+    console.log(coffee)
+    return {
+        ...member,
+        coffee:coffee
+    }
+ })
+}
+
+const getMember = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const member = family.find(res => res.id === id)
+            if (member) {
+                resolve(member)
+            } else {
+                reject(Error('Member is not find!'))
+            }
+        }, 1500)
+    })
+}
+
+getMember(2)
+.then(data=>{
+   return getCoffee(data);
+})
+.then(newMember => {
+    console.log('newMember > ', newMember)
+})
+.catch(error => console.log(error))
