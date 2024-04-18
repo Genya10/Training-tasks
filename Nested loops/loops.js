150bf9c224eb098b80949783ea665940ca9dbf8c
Jenya <113167825+Genya10@users.noreply.github.com>
2024-04-18 09:54:57 +0300

    add 2 tasks with loops again

--- Nested loops/loops.js
+++ Nested loops/loops.js
@@ -19,13 +19,39 @@ console.log(out);
 document.querySelector('.out').innerHTML = out;
 
 /////////////////////////
-/*let put = '';
-for (let i = 5; i => 0; i--) {
-    for (let j = i; j => 0; j--) {
+let put = '';
+for (let i = 5; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
         put += '*'
     }
     put += '<br>'
 }
 console.log(put);
 
-document.querySelector('.put').innerHTML = put;*/
+document.querySelector('.put').innerHTML = put;
+
+////////////////////////////
+let put_1 = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 7; j++) {
+        if (j <= i) put_1 += "*"
+        else put_1 += '-'
+    }
+    put_1 += '<br>'
+}
+document.querySelector('.put-1').innerHTML = put_1;
+
+//////////////////////////////
+let put_2 = '';
+let p = 1;
+let s = 16;
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 18; j++) {
+        if (j >= p && j <= s) put_2 += '* '
+        else put_2 += '_'
+    }
+    p = p + 2;
+    s = s - 2;
+    put_2 += '<br>'
+}
+document.querySelector('.put-2').innerHTML = put_2;
