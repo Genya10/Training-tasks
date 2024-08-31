66f80b316a1da181ef2388b7da917ef5b28341e3
Jenya <113167825+Genya10@users.noreply.github.com>
2024-08-31 15:26:22 +0300

    add tsks with callback, methods map and reduce

--- Tasks array/scriptEight.js
+++ Tasks array/scriptEight.js
@@ -21,3 +21,46 @@ console.log(formateddNumber)
 
 const formateddNumber1 = number.toLocaleString('uk-UK', {style:'currency', currency:'USD'})
 console.log(formateddNumber1)
+
+//reduce
+const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"]
+const repeatFruits = fruits.reduce((accum, value) => {
+    if(accum[value]){
+        accum[value]++
+    } else {
+        accum[value] = 1
+    }
+    return accum
+},
+{})
+console.log(repeatFruits)
+
+
+//callback
+function greeting(name, word){
+    console.log('Hello ' + name + ' I am ' + word + '!')
+}
+
+function resultGreeting(callback){
+    const greet = 'World'
+    const feel = ' happy'
+    return callback(greet, feel)
+}
+
+resultGreeting(greeting)
+
+//map
+const freshFruits = ["apple", "banana", "cherry", "date"]
+const fruitsMap = freshFruits.map(str => str.length)
+console.log(fruitsMap)
+
+//map
+const animals = ["dog", "cat", "bird"]
+const animalsMap = animals.map(animal => {
+    return 'pet ' + animal
+})
+console.log(animalsMap)
+
+//map
+const numbers1 = [2, 4, 6, 8, 10]
+console.log(numbers1.map(num => num *num * num))
